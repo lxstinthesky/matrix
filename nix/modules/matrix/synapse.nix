8e186829cab183a031873f0a69780b1ff1fb773f
Henrik <h.jentgens@web.de>
2026-03-22 21:16:33 +0100

    simple sysnapse setup with psql database

--- nix/modules/matrix/synapse.nix
+++ nix/modules/matrix/synapse.nix
@@ -1,5 +1,52 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
+  services.postgresql = {
+    enable = true;
+    enableTCPIP = false; # only using unix sockets
+    package = pkgs.postgresql_17; 
+    initialScript = pkgs.writeText "synapse-init.sql" ''
+      CREATE ROLE "matrix-synapse" LOGIN;
+      CREATE DATABASE "matrix-synapse"
+        WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = 'C'
+        LC_CTYPE = 'C';
+    '';
+    settings = {
+      unix_socket_directories = "/run/postgresql";
+    };
+  };
 
+  # setting up unix socket directory for postgresql
+  systemd.tmpfiles.rules = [
+   "d /run/postgresql 0755 postgres postgres -"
+  ];
+
+  services.matrix-synapse = {
+    enable = true;
+    settings.server_name = "matrix.com";
+    settings.public_baseurl = "http://test.matrix.com";
+    # The public base URL value must match the `base_url` value set in `clientConfig` above.
+    # The default value here is based on `server_name`, so if your `server_name` is different
+    # from the value of `fqdn` above, you will likely run into some mismatched domain names
+    # in client applications.
+    settings.database.args = {
+      user = "matrix-synapse";
+    };
+    
+
+    settings.listeners = [
+      { port = 8008;
+        bind_addresses = [ "127.0.0.1" ];
+        type = "http";
+        tls = false;
+        x_forwarded = true;
+        resources = [ {
+          names = [ "client" "federation" ];
+          compress = true;
+        } ];
+      }
+    ];
+  };
 }
