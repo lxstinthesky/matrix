3cda6f1ec874a504f238c12c50304f49d608f754
Henrik <h.jentgens@web.de>
2026-03-29 14:59:54 +0200

    upgrading to nixos version 25.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     # stable and unstable nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     
     # partitioning and disk management
