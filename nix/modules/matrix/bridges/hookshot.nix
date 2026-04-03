605b948d98f3db6744fc5c4c26b7ccfc3b5cba7d
Henrik <h.jentgens@web.de>
2026-04-03 18:35:39 +0200

    fixing hookshot

--- nix/modules/matrix/bridges/hookshot.nix
+++ nix/modules/matrix/bridges/hookshot.nix
@@ -92,41 +92,41 @@ in
 			User = "root";
 			Group = "root";
 		};
-		script = ''
-			install -d -m 0750 -o ${hookshotUser} -g ${hookshotGroup} /var/lib/matrix-hookshot
-
-			if [ ! -f ${registrationFile} ]; then
-				as_token="$(${pkgs.openssl}/bin/openssl rand -hex 32)"
-				hs_token="$(${pkgs.openssl}/bin/openssl rand -hex 32)"
-
-				cat > ${registrationFile} <<'EOF'
-		id: hookshot
-		url: http://127.0.0.1:9993
-		as_token: __AS_TOKEN__
-		hs_token: __HS_TOKEN__
-		sender_localpart: hookshot
-		rate_limited: false
-		namespaces:
-		  rooms: []
-		  aliases: []
-		  users:
-		    - regex: '@hookshot:${domainRegex}'
-		      exclusive: true
-		    - regex: '@feeds:${domainRegex}'
-		      exclusive: true
-		    - regex: '@_webhooks_.*:${domainRegex}'
-		      exclusive: true
-		EOF
-
-				${pkgs.gnused}/bin/sed -i \
-					-e "s|__AS_TOKEN__|$as_token|" \
-					-e "s|__HS_TOKEN__|$hs_token|" \
-					${registrationFile}
-			fi
-
-			chown ${hookshotUser}:matrix-synapse ${registrationFile}
-			chmod 0440 ${registrationFile}
-		'';
+		script = lib.concatStringsSep "\n" [
+			"install -d -m 0750 -o ${hookshotUser} -g ${hookshotGroup} /var/lib/matrix-hookshot"
+			""
+			"if [ ! -f ${registrationFile} ]; then"
+			"\tas_token=\"$(${pkgs.openssl}/bin/openssl rand -hex 32)\""
+			"\ths_token=\"$(${pkgs.openssl}/bin/openssl rand -hex 32)\""
+			""
+			"\tcat > ${registrationFile} <<'EOF'"
+			"id: hookshot"
+			"url: http://127.0.0.1:9993"
+			"as_token: __AS_TOKEN__"
+			"hs_token: __HS_TOKEN__"
+			"sender_localpart: hookshot"
+			"rate_limited: false"
+			"namespaces:"
+			"  rooms: []"
+			"  aliases: []"
+			"  users:"
+			"    - regex: '@hookshot:${domainRegex}'"
+			"      exclusive: true"
+			"    - regex: '@feeds:${domainRegex}'"
+			"      exclusive: true"
+			"    - regex: '@_webhooks_.*:${domainRegex}'"
+			"      exclusive: true"
+			"EOF"
+			""
+			"\t${pkgs.gnused}/bin/sed -i \\\""
+			"\t\t-e \"s|__AS_TOKEN__|$as_token|\" \\\""
+			"\t\t-e \"s|__HS_TOKEN__|$hs_token|\" \\\""
+			"\t\t${registrationFile}"
+			"fi"
+			""
+			"chown ${hookshotUser}:matrix-synapse ${registrationFile}"
+			"chmod 0440 ${registrationFile}"
+		];
 	};
 
 	services.nginx.virtualHosts."${fqdn}" = {
